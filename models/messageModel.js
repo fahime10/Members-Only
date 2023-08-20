9d1d12155caf1f1f9c8d1ac0787a575c956c2c47
Fahim Ahmed <p2595607@my365.dmu.ac.uk>
2023-08-20 01:11:06 +0100

    Added model and controller classes

---
+++ models/messageModel.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const { DateTime } = require('luxon');
+
+const Schema = mongoose.Schema;
+
+const MessageSchema = new Schema({
+    username: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    title: { type: String, required: true, minLength: 1, maxLength: 100 },
+    text: { type: String, required: true, minLength: 1, maxLength: 1000 },
+    timestamp: { type: Date, default: Date.now, required: true } 
+});
+
+MessageSchema.virtual('date').get(function () {
+    return DateTime.fromJSDate(this.timestamp).toFormat('yyyy-MM-dd, HH:mm');
+});
+
+module.exports = mongoose.model('Message', MessageSchema);
---
+++ routes/routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const index_controller = require("../controllers/indexController");
+const user_controller = require("../controllers/userController");
+const message_controller = require("../controllers/messageController");
+
+
+
+module.exports = router;
